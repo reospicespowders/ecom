71925ebcd7c6e09b104413240e740ea79d88bf79
Reorank <bottleneckcalculatorpc@gmail.com>
2025-06-20 11:49:02 +0500

    testtt

--- next.config.js
+++ next.config.js
@@ -10,14 +10,6 @@ const nextConfig = {
       },
     ],
   },
-  async rewrites() {
-    return [
-      {
-        source: '/studio/:path*',
-        destination: 'http://localhost:3333/:path*',
-      },
-    ];
-  },
 }
 
 module.exports = nextConfig
