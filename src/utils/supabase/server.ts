c82d09defd90ccb6fdab8346c9ee6c1deb8aa1df
Reorank <bottleneckcalculatorpc@gmail.com>
2025-06-26 02:59:25 +0500

    fixning2

--- src/utils/supabase/server.ts
+++ src/utils/supabase/server.ts
@@ -1,25 +1,14 @@
 import { createClient } from '@supabase/supabase-js'
-import { auth } from '@clerk/nextjs/server'
 
 export const createClerkSupabaseClient = () => {
   return createClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    process.env.SUPABASE_SERVICE_ROLE_KEY! || process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
-      global: {
-        fetch: async (url, options = {}) => {
-          const { getToken } = await auth();
-          const supabaseToken = await getToken();
-
-          const headers = new Headers(options?.headers);
-          headers.set('Authorization', `Bearer ${supabaseToken}`);
-          
-          return fetch(url as RequestInfo, {
-            ...options,
-            headers,
-          });
-        },
-      },
+      auth: {
+        autoRefreshToken: false,
+        persistSession: false
+      }
     }
   );
 }; 
